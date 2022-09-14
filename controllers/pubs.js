071c5500c2f918b0cbc6543310e9d399d73d2e6d
elek8871 <elekmichelle@gmail.com>
2022-09-14 13:20:57 -0600

    updates routes

--- controllers/pubs.js
+++ controllers/pubs.js
@@ -1,29 +1,54 @@
+// REQUIRED THINGS
 const express =require("express")
 const router = express.Router()
 const db= require("../models")
 const crypto = require("crypto-js")
 const bcrypt= require("bcrypt")
+const { default: axios } = require("axios")
 
-// GET/pubs  shows a list of pubs based on user queries *route works
+//************  ROUTES TO SEARCH FOR PUBS AND DISPLAY A LIST OF PUBS  ******************
+
+// GET/pubs  shows a list of pubs based on user queries 
 router.get("/index", (req, res)=>{
-    res.send('List of pubs')
-    // res.render("users/new.ejs")
     // axios call and save response.data
-  
+    axios.get("https://api.openbrewerydb.org/breweries?by_city=denver&per_page=3")
+        .then (response =>{
+            res.render('index.ejs', {pubs:response.data})
+        })
+        .catch(console.log) 
 })
 //  GET /pubs/:id lists information about a specific pub *route works
 router.get ("/:id", (req, res)=>{
     res.send("List details ")
 })
 
-// POST /pubs/new shows a form allowing a user to their favorites
-router.post ("/new", (req,res)=>{
-    res.send ("Form allowing user to add pub to their fav")
+//********* ROUTES TO ADD AND EDIT USER FAVORITES **************************
+
+// GET/ faves READs all favorited pubs and displays to the user
+router.get ("/favs", async (req, res)=>{
+    try{
+        const allFavs = await db.fave.findAll()
+        res.render("favs.ejs",{
+            allFavs
+        })
+    } catch(error){
+        console.log(error)
+        res.send("server error")
+    }
+})
+// POST /pubs/favs - creates a new fave and redirect user to display user favs
+router.post ("/new",async (req,res)=>{
+    try{
+        console.log(req.body)
+    }catch(error){
+        console.log(error)
+        res.send("server error")
+    }
 })
 
 // PUT /pubs/edit/:id shows a form that allows a user to comment on their fav pubs
 router.put ("/edit:id", (req,res)=>{
-    res.send ("Allows user to update pub info")
+  
 })
 
 // DELETE /pubs/:id allows the user to delete the pub from their favorites 
